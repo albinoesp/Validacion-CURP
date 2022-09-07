899e99423384077019f81c7fd2d8118c417fc7ec
AlbinoJ <jesusdavida.e2089@gmail.com>
2022-09-07 11:25:57 -0700

    Validación CURP Status: En proceso

--- src/validarCURP.java
+++ src/validarCURP.java
@@ -1,2 +1,20 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 public class validarCURP {
+    private static final Logger LOG = Logger.getLogger("validate.validarCURP");
+
+    private static void validar(String args[]){
+
+        LOG.setLevel(Level.WARNING);
+
+        if (args.length == 0){
+            LOG.severe("No se proporciono la CURP");
+            System.exit(1);
+        }
+
+        String CURP = args[0];
+
+
+    }
 }
